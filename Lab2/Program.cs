5b47a7a2c5229535f9826fc47d5ab0c9c848851a
Ivan Lopatin <vaneklopatin.main@gmail.com>
2024-10-08 22:28:43 +0300

    lab 2 is completed but not debugged

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,11 +1,93 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lab2
 {
+
     internal class Program
     {
+        #region Constants
+        private const int N = 25;
+        private const double INITIAL_TEMPERATURE = 30.0;
+        private const double FINAL_TEMPERATURE = 0.05;
+        private const double ALFA = 0.98;
+        private const int ITERATION_COUNT = 100;
+        #endregion
+        public class Solution
+        {
+            public double Energy { get; set; }
+            public List<int[]> Edges { get; set; }
+            public int[] Vector { get; set; }
+
+            public Solution(List<int[]> edges)
+            {
+                this.Edges = edges;
+                this.Vector = new int[edges.Count];
+                // Генерация начального решения
+                for (int i = 0; i < this.Edges.Count; i++)
+                {
+                    //if (this.Edges[i][1] != N - 1)
+                        this.Vector[i] = this.Edges[i][2];
+                    //else this.Vector[i] = 0;
+                }
+                this.DefineEnergy();
+            }
+
+            private void DefineEnergy()
+            {
+                int energy = 0;
+                for (int i = 1; i < N - 1; i++)
+                {
+                    int inputs = 0;
+                    int outputs = 0;
+                    for (int j = 0; j < this.Edges.Count; j++)
+                    {
+                        if (this.Edges[j][0] == i)
+                        {
+                            inputs += this.Vector[i];
+                        }
+                        else if (this.Edges[j][1] == i)
+                        {
+                            outputs += this.Vector[i];
+                        }
+                    }
+                    energy += Math.Abs(inputs - outputs);
+                }
+                this.Energy = energy;
+            }
+
+            public void Randomize()
+            {
+                Random random = new Random();
+                int index = random.Next(0, this.Edges.Count);
+                this.Vector[index] = random.Next(0, this.Edges[index][2]);
+                this.DefineEnergy();
+            }
+
+            public Solution DeepCopy()
+            {
+                string json = JsonConvert.SerializeObject(this);
+                Solution? copy = JsonConvert.DeserializeObject<Solution>(json);
+                if (copy == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                return copy;
+            }
+
+        }
+
+        
+
+        private static Random random = new Random();
         static void Main(string[] args)
         {
+            double EvaluateSolution(Solution workingSolution, Solution currentSolution, double temperature)
+            {
+                return Math.Exp(-(workingSolution.Energy - currentSolution.Energy) / temperature);
+            }
+
             int[,] GenerateAdjacencyMatrix(int n)
             {
                 // Создаем матрицу смежности размером n x n и заполняем ее нулями
@@ -23,9 +105,9 @@ namespace Lab2
                 // Заполняем остальные элементы матрицы случайными весами
                 for (int i = 0; i < n; i++)
                 {
-                    for (int j = i + 2; j < n; j++)
+                    for (int j = 0; j < n; j++)
                     {
-                        if (random.Next(2) == 1) // С вероятностью 0.5 добавляем ребро
+                        if (random.Next(2) == 1 && i != j && j != i + 1 && i != n - 1 && j != 0) // С вероятностью 0.5 добавляем ребро
                         {
                             adjacencyMatrix[i, j] = random.Next(1, 11);
                         }
@@ -35,10 +117,10 @@ namespace Lab2
                 return adjacencyMatrix;
             }
 
-            List<ArrayList> GenerateEdgeList(int[,] adjacencyMatrix)
+            List<int[]> GenerateEdgeList(int[,] adjacencyMatrix)
             {
                 int n = adjacencyMatrix.GetLength(0);
-                List<ArrayList> edgeList = new List<ArrayList>();
+                List<int[]> edgeList = new List<int[]>();
 
                 for (int i = 0; i < n; i++)
                 {
@@ -46,10 +128,10 @@ namespace Lab2
                     {
                         if (adjacencyMatrix[i, j] > 0)
                         {
-                            ArrayList edge = new ArrayList();
-                            edge.Add(i); // Исходящая вершина
-                            edge.Add(j); // Входящая вершина
-                            edge.Add(adjacencyMatrix[i, j]); // Вес ребра
+                            int[] edge = new int[3];
+                            edge[0] = i; // Исходящая вершина
+                            edge[1] = j; // Входящая вершина
+                            edge[2] = adjacencyMatrix[i, j]; // Вес ребра
                             edgeList.Add(edge);
                         }
                     }
@@ -58,8 +140,8 @@ namespace Lab2
                 return edgeList;
             }
 
-            int[,] matrix = GenerateAdjacencyMatrix(10); // Создаем матрицу для графа с 5 вершинами
-            List<ArrayList> edgeList = GenerateEdgeList(matrix);
+            int[,] matrix = GenerateAdjacencyMatrix(N); // Создаем матрицу для графа с 5 вершинами
+            List<int[]> edgeList = GenerateEdgeList(matrix);
             // Выводим матрицу на консоль (для примера)
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
@@ -70,11 +152,45 @@ namespace Lab2
                 Console.WriteLine();
             }
             Console.WriteLine("=================");
-            foreach (ArrayList edge in edgeList)
+            foreach (int[] edge in edgeList)
             {
                 Console.WriteLine("Исходная вершина: " + edge[0] + ", Входящая вершина: " + edge[1] + ", Вес: " + edge[2]);
             }
 
+            Solution currentSolution = new Solution(edgeList);
+            Solution workingSolution = currentSolution.DeepCopy();
+            Solution bestSolution = currentSolution.DeepCopy();
+            double T = INITIAL_TEMPERATURE;
+
+            while (T > FINAL_TEMPERATURE && bestSolution.Energy != 0)
+            {
+                for (int i = 0; i < ITERATION_COUNT; i++)
+                {
+                    workingSolution.Randomize();
+                    if (workingSolution.Energy <= currentSolution.Energy)
+                    {
+                        currentSolution = workingSolution.DeepCopy();
+                        if (currentSolution.Energy < bestSolution.Energy)
+                        {
+                            bestSolution = currentSolution.DeepCopy();
+                        }
+                    }
+                    else if (random.NextDouble() < EvaluateSolution(workingSolution, currentSolution, T))
+                    {
+                        currentSolution = workingSolution.DeepCopy();
+                        if (currentSolution.Energy < bestSolution.Energy)
+                        {
+                            bestSolution = currentSolution.DeepCopy();
+                        }
+                    }
+                    else
+                    {
+                        workingSolution = currentSolution.DeepCopy();
+                    }
+                }
+                Console.WriteLine($"T = {Math.Round(T, 7)} | Энергия = {bestSolution.Energy}");
+                T *= ALFA;
+            }
 
         }
 
